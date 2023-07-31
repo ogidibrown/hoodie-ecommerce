32ed9b84dc5e3c86f20ed74113a5f9a3d271e4f9
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-07-31 11:20:31 +0000

    home slide

---
+++ client/src/app/components/Carousel.jsx
@@ -0,0 +1,42 @@
+'use client'
+import React, { useState } from 'react'
+import { ChevronLeft, ChevronRight } from 'react-feather'
+
+export default function Carousel  ({children:Slides}) {
+
+    const [curr, setCur] = useState(0)
+
+    const prev = ()=>{
+        setCur((curr) => (curr === 0 ? Slides.length -1 : curr - 1))
+    }
+    const next = ()=>{
+        setCur((curr) => (curr === Slides.length -1 ? 0 : curr + 1))
+    }
+
+  return (
+    <div className=' w-full bg-cover overflow-hidden relative'>
+        <div className=' flex transition-transform bg-cover bg-center ease-out duration-500' style={{transform:`translateX(-${curr*100}%)`}}>{Slides}</div>
+        <div className='absolute  inset-0 flex items-center justify-between md:mb-10 text-white'>
+            <button onClick={prev} className='bg-black flex items-center rounded-full shadow mb-5 ml-3 cursor-pointer '>
+              <ChevronLeft size={40} />
+            </button>
+            <div className='flex flex-col items-center md:min-h-screen md:mt-10 gap-10 md:gap-2 justify-evenly'>
+                <div className="flex justify-center items-center ">
+                    <h1 className=" text-[20px] md:text-[60px] font-semibold text-white text-center  md:leading-[90px]  uppercase">
+                        "Let's explore a treasure <br/> 
+                        trove of <span className='text-[#0F8649]'>uniquely designed</span> <br/>
+                        hoodies."
+                        </h1> 
+                </div>
+                <div className='flex justify-center items-center mt-4 md:mb-[70px]'>
+                        <button className='text-white text-[20px]  md:text-[33px] w-[180px] h-[40px]  md:w-[280px] md:h-[60px] font-bold text-center bg-black'>SHOP NOW</button>
+                </div>
+            </div>
+            <button onClick={next} className='bg-black flex items-center rounded-full shadow mb-5 mr-3 cursor-pointer '>
+              <ChevronRight size={40} />
+            </button>
+            
+        </div>
+    </div>
+  )
+}
--- client/src/app/sections/HeroSection.jsx
+++ client/src/app/sections/HeroSection.jsx
@@ -1,20 +1,26 @@
+'use client'
 import Image from 'next/image'
+import Carousel from '../components/Carousel'
+
+
+
+
+const Slides = [
+    "/hero1.png",
+    "/hero2.png",
+    "/hero3.png",
+    "/hero4.png"
+]
 
 const HeroSection = () => {
   return (
-    <div className="min-h-screen bg-cover bg-center bg-opacity-50" style={{ backgroundImage: "url('/hero1.png')" }}>
-        <div className='flex flex-col items-center min-h-screen  justify-evenly'>
-            <div className="flex justify-center items-center h-full">
-                <h1 className="text-[60px] font-semibold text-white text-center leading-[90px]  [text-shadow_10px_rgb(0_0_0_/_40%)] uppercase">
-                    "Let's explore a treasure <br/> 
-                    trove of <span className='text-[#0F8649]'>uniquely designed</span> <br/>
-                    hoodies."
-                    </h1> 
-            </div>
-            <div className='flex justify-center items-center mb-[70px]'>
-                    <button className='text-white text-[33px]  w-[280px] h-[60px] font-bold text-center bg-black'>SHOP NOW</button>
-            </div>
-        </div>
+    <div className="">
+        <Carousel >
+            {Slides.map((s)=>(
+                <Image src={s} width={900} height={900} alt="hero images" className=' h-[400px] md:min-h-screen md:min-w-full  w-full ' />
+            ))}
+            
+        </Carousel>
     </div>
   )
 }
