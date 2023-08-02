0915a48a42f2191556b5abdd43814dbd3de28ef4
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-08-02 09:15:51 +0000

    about us

--- client/src/app/about/page.js
+++ client/src/app/about/page.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
 import AboutImg from 'public/about.png';
+import Stars from 'public/stars.png';
 import Link from 'next/link';
 
 
 const About = () => {
   return (
     <div className='mx-[20px] mb-[30px]'>
-        <div className='w-full relative h-[800px]'>
+        <div className='w-full relative h-[930px] md:h-[800px]'>
             <h1 className='md:text-[38px] flex justify-center mb-[30px] font-bold'>About Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Tenetur maxime assumenda est mollitia labore possimus 
@@ -26,16 +27,69 @@ const About = () => {
                 praesentium possimus eum incidunt, magni similique!
             </p>
             <div className='grid grid-cols-2 place-items-center gap-1 md:grid-cols-3 '>
-                <div className='flex flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[480px]'>
+                <div className='flex flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[330px] md:h-[460px]'>
                     <div className='bg-[#E9E9E9] '>
                         <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  />
                     </div>
-                    <div>
-                        ggjgasgdgjadgjha
+                    <div className='flex flex-col h-[200px] p-3 justify-evenly '>
+                        <div>
+                            <p className='text-[13px] md:text-base'>UNISEX hEAVY BLEND HOODIE |
+                                <br />
+                            GLIDAN 13000
+                            </p>
+                        </div>
+                        <div className='flex flex-col md:flex-row md:gap-3 items-center'>
+                             <Image src={Stars} alt='stars image' width={150} height={150} className='object-cover'  />
+                             <p>299 reviews</p>
+                        </div>
+                        <div className=' md:pt-[20px] pt-[5px]'>
+                            <h1 className='text-[13px] md:text-base font-bold' >Starting from $70.75</h1>
+                        </div>
+                        
                     </div>
                 </div>
-                <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  /> 
-                <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover md:flex hidden'  />
+                <div className='flex flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[330px] md:h-[460px]'>
+                    <div className='bg-[#E9E9E9] '>
+                        <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  />
+                    </div>
+                    <div className='flex flex-col h-[200px] p-3 justify-evenly '>
+                        <div>
+                            <p className='text-[13px] md:text-base'>UNISEX hEAVY BLEND HOODIE |
+                                <br />
+                            GLIDAN 13000
+                            </p>
+                        </div>
+                        <div className='flex flex-col md:flex-row md:gap-3 items-center'>
+                             <Image src={Stars} alt='stars image' width={150} height={150} className='object-cover'  />
+                             <p>299 reviews</p>
+                        </div>
+                        <div className=' md:pt-[20px] pt-[5px]'>
+                            <h1 className='text-[13px] md:text-base font-bold' >Starting from $70.75</h1>
+                        </div>
+                        
+                    </div>
+                </div>
+                <div className='flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[330px] md:h-[460px] hidden md:flex'>
+                    <div className='bg-[#E9E9E9] '>
+                        <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  />
+                    </div>
+                    <div className='flex flex-col h-[200px] p-3 justify-evenly '>
+                        <div>
+                            <p className='text-[13px] md:text-base'>UNISEX hEAVY BLEND HOODIE |
+                                <br />
+                            GLIDAN 13000
+                            </p>
+                        </div>
+                        <div className='flex flex-col md:flex-row md:gap-3 items-center'>
+                             <Image src={Stars} alt='stars image' width={150} height={150} className='object-cover'  />
+                             <p>299 reviews</p>
+                        </div>
+                        <div className=' md:pt-[20px] pt-[5px]'>
+                            <h1 className='text-[13px] md:text-base font-bold' >Starting from $70.75</h1>
+                        </div>
+                        
+                    </div>
+                </div>
             </div>
             <div className='absolute  left-[20px] bg-[#53c28bc4] p-[5px] text-white '>
                 <h1>Digital Shopping</h1>
--- client/src/app/contact/page.js
+++ client/src/app/contact/page.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import Image from 'next/image'
+import AboutImg from 'public/about.png';
+import Stars from 'public/stars.png';
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div className='mx-[20px] md:mx-[40px] mb-[30px]'>
+        <div>
+            <h1 className='md:text-[38px] flex justify-center mb-[30px] font-bold'>Contact Us</h1>
+        </div>
+        <div className='flex'>
+            <div className='flex justify-center flex-1'>
+                    <div className='flex flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[330px] md:h-[460px]'>
+                            <div className='bg-[#E9E9E9] '>
+                                <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  />
+                            </div>
+                            <div className='flex flex-col h-[200px] p-3 justify-evenly '>
+                                <div>
+                                    <p className='text-[13px] md:text-base'>UNISEX hEAVY BLEND HOODIE |
+                                        <br />
+                                    GLIDAN 13000
+                                    </p>
+                                </div>
+                                <div className='flex flex-col md:flex-row md:gap-3 items-center'>
+                                    <Image src={Stars} alt='stars image' width={150} height={150} className='object-cover'  />
+                                    <p>299 reviews</p>
+                                </div>
+                                <div className=' md:pt-[20px] pt-[5px]'>
+                                    <h1 className='text-[13px] md:text-base font-bold' >Starting from $70.75</h1>
+                                </div>
+                                
+                            </div>
+                    </div>
+            </div>
+            <div className='flex justify-center flex-1'>
+                Lorem, ipsum dolor sit amet consectetur
+                 adipisicing elitjfygkhlkjkkkkkkkkkkkkkkkkkk
+                 jjjjjjjjjjjjjjvjhfhjjhfjgmnhhhvhvjmbj,n,
+                 hgcvhbmbjjhvnbmnbjnbnnvbmbmnv bv cnbkgncgf
+                 hccghgjk,hjhcbxbbvnbgnbchhmbjb g
+
+            </div>
+        </div>
+    </div>
   )
 }
 
