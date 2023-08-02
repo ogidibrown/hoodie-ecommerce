20bfad8b54895fee8813aa63474442ef14e4268a
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-08-02 08:13:33 +0000

    about

---
+++ client/src/app/about/page.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import Image from 'next/image'
+import AboutImg from 'public/about.png';
+import Link from 'next/link';
+
+
+const About = () => {
+  return (
+    <div className='mx-[20px] mb-[30px]'>
+        <div className='w-full relative h-[800px]'>
+            <h1 className='md:text-[38px] flex justify-center mb-[30px] font-bold'>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Tenetur maxime assumenda est mollitia labore possimus 
+                quibusdam facere sed! Deserunt illum consectetur, optio 
+                voluptas numquam ipsum non possimus rem tempore iure itaque 
+                amet sunt. Ea laboriosam earum sit voluptates fuga similique 
+                repudiandae placeat ipsam accusantium consectetur rem ullam, 
+                velit delectus explicabo id odit consequuntur reprehenderit, 
+                reiciendis quos vel officia. Voluptatem, temporibus expedita 
+                quae qui iure officiis a accusantium, incidunt animi tempore 
+                <br />
+                <br />
+                debitis magni eaque numquam rem, quasi excepturi voluptas dolor 
+                doloremque enim doloribus corporis ad veritatis molestiae possimus? 
+                Totam perspiciatis dicta debitis, voluptates, consequuntur commodi 
+                praesentium possimus eum incidunt, magni similique!
+            </p>
+            <div className='grid grid-cols-2 place-items-center gap-1 md:grid-cols-3 '>
+                <div className='flex flex-col m-3 rounded-[4px] border-[#0F8649] border-2 h-[480px]'>
+                    <div className='bg-[#E9E9E9] '>
+                        <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  />
+                    </div>
+                    <div>
+                        ggjgasgdgjadgjha
+                    </div>
+                </div>
+                <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover'  /> 
+                <Image src={AboutImg} alt='about image' width={300} height={300} className='object-cover md:flex hidden'  />
+            </div>
+            <div className='absolute  left-[20px] bg-[#53c28bc4] p-[5px] text-white '>
+                <h1>Digital Shopping</h1>
+                <h2>Handcrafting award winning digital experience</h2>
+            </div>
+        </div>
+        <div className='flex gap-4'>
+            <div className='flex-1 text-justify  mt-[20px] gap-2'>
+                <h1 className='font-bold text-2xl mb-2'>Who are We?</h1>
+                <p>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellat ratione nostrum sequi obcaecati iste enim in accusantium rerum ut,
+                    quod, minus, accusamus officia harum! Explicabo provident ipsum iste ipsa repellat
+                    <br/>
+                    <br/>
+
+                    ipsam iure adipisci itaque beatae porro provident eaque reprehenderit voluptas quam,
+                    harum sit aspernatur pariatur quos sequi nostrum. Adipisci obcaecati natus delectus 
+                </p>
+            </div>
+            <div className=' mt-[20px] flex-1 gap-2 text-justify .' >
+            <h1 className='font-bold text-2xl mb-2'>What We Do</h1>
+                <p className='mb-[20px]'>
+                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Repellat ratione nostrum sequi obcaecati iste enim in accusantium rerum ut,
+                    quod, minus
+                    <br/>
+                    <br/> -Dynamic Shoppiing
+                    <br/> -Fast and Handy 
+                    <br/> -All kinds of Hodies
+                                
+                </p>
+                <Link href='/contact' >  
+                    <button className='w-[100px] h-[40px] bg-[#0F8649] text-white rounded-[3px]'>Contact Us</button>
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- client/src/app/components/navbar/navbar.jsx
+++ client/src/app/components/navbar/navbar.jsx
@@ -32,20 +32,20 @@ const Navbar = () => {
                             <Link href="/" >Download</Link>
                         </li>
                         <li className='flex text-[12px] md:text-[16px]'>
-                            <span>About us</span>
+                        <Link href="/about" >About Us</Link>
                         </li>
                     </ul>
                 </div>
                 <div className=''>
                     <ul className='flex gap-4 justify-center items-center'>
                         <li className='flex text-[12px] md:text-[16px]'>
-                            <Link href="/" >Contact Us</Link>
+                            <Link href="/contact" >Contact Us</Link>
                         </li>
                         <li className='flex text-[12px] md:text-[16px]'>
-                            <Link href="/" >Register</Link>
+                            <Link href="/signup" >Register</Link>
                         </li>
                         <li className='flex text-[12px] md:text-[16px]'>
-                            <Link href="/" >Login</Link>
+                            <Link href="/login" >Login</Link>
                         </li>
                     </ul>
                 </div>
@@ -54,13 +54,12 @@ const Navbar = () => {
         <div className='flex justify-between items-center'>
             <div className='flex items-center gap-1 md:gap-2'>
                 < FaBars  className='text-[#111] md:w-[25px] md:h-[25px] cursor-pointer ' onClick={()=>dispatch(setSidebarOn())}  />
-               < BsFillBagFill className='text-[#111] md:w-[25px] md:h-[25px]' />
-               <Link href="/" className='flex items-center' ><Image src={Logo} width={100} height={100} className='flex items-center pt-1' /></Link>
+               <Link href="/" className='flex items-center' ><Image src={Logo} width={190} height={100} className='flex items-center h-[100px] w-[100px] md:h-[170px] md:w-[190px] mr-2 pt-1'  /></Link>
 
             </div>
             <div className='flex flex-col' >
-                <div className=' bg-white flex  w-[150px] md:w-[810px] items-center border-4 border-[#0F8649] rounded-[3px] '>
-                    <input type="text" placeholder='Search your prefered product here...' className='w-[110px] p-2 md:w-[800px] md:h-[40px]  h-[30px] border-none outline-none text-[10px] md:text-[15px] text-[#111] rounded-[5px]' />
+                <div className=' bg-white flex  w-[200px] md:w-[880px] items-center border-4 border-[#0F8649] mr-3 rounded-[3px] '>
+                    <input type="text" placeholder='Search your prefered product here...' className='w-[170px] p-2 md:w-[800px] md:h-[40px]  h-[30px] border-none outline-none text-[10px] md:text-[15px] text-[#111] rounded-[5px]' />
                     <Link href='' className='md:w-[60px] w-[40px] h-[22px] mr-[4px] md:h-[30px] bg-[#0F8649]  rounded-[3px] flex items-center justify-center'>
                         <ImSearch className='text-[#fff]' />
                     </Link>
